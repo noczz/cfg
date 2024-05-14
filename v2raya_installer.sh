79b7c3fd8d51bbf13890188c2092aa1736a29516
noczz <61116217+noczz@users.noreply.github.com>
2024-05-15 07:14:39 +0800

    更新 v2raya_installer.sh
    安装v2raya

---
+++ v2raya_installer.sh
@@ -0,0 +1,3 @@
+wget -qO - https://apt.v2raya.org/key/public-key.asc | sudo tee /etc/apt/keyrings/v2raya.asc
+echo "deb [signed-by=/etc/apt/keyrings/v2raya.asc] https://apt.v2raya.org/ v2raya main" | sudo tee /etc/apt/sources.list.d/v2raya.list
+sudo apt update
